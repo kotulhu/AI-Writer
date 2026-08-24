d093e6e6c8cb51b055b72ea435557b3f49be7544
khtulhu <speranza.ua@gmail.com>
2026-08-24 16:07:20 +0300

    Initial Commit

---
+++ AI writer/AI_writerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_writerApp.swift
+//  AI writer
+//
+//  Created by El Gato on 24.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct AI_writerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI writer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI writer
+//
+//  Created by El Gato on 24.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
